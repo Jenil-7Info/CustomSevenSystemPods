341db0e7a70548aae2f26ddb49e7b15ed1824530
Jenil Rughani <jenil@seveninfosystem.com>
2023-11-29 11:16:18 +0530

    Update Example

--- Example/CustomSevenSystemPods/ViewController.swift
+++ Example/CustomSevenSystemPods/ViewController.swift
@@ -7,18 +7,16 @@
 //
 
 import UIKit
+import CustomSevenSystemPods
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(printerStatement("My First Custom Pods."))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 }
-
