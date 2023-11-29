a42d6869101037639b3de9e81b34e1fd789eaddb
Jenil Rughani <jenil@seveninfosystem.com>
2023-11-29 16:46:06 +0530

    Add New function

--- CustomSevenSystemPods.swift
+++ CustomSevenSystemPods.swift
@@ -5,3 +5,8 @@ import UIKit
 public func printerStatement(_ statement: String) -> String {
     return "Your Statement is: \(statement)"
 }
+
+//MARK: - Changer
+public func changerValue(_ digit: Int) -> String {
+    return "\(digit)"
+}
